b490fbce2a40980c31e514d83238df1c5971499f
cw-dhb <20020900@coway.com>
2022-03-17 17:23:42 +0900

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  
+  return <>
+  <Script src="//dapi.kakao.com/v2/maps/sdk.js?appkey=f6991cb3c19e49f617d52a93a801bd32&autoload=false&libraries=services,clusterer,drawing" strategy='beforeInteractive'/>
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
